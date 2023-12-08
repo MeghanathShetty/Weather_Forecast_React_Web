ba6941ef2c635f24e3cf3e91654ec95355a02a9d
Meghanath Shetty <meghanshetty3122@gmail.com>
2023-12-08 16:21:15 +0530

    commit

--- next.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    // Add exportPathMap configuration
-    exportPathMap: function () {
-      return {
-        '/': { page: '/' },
-        // Add other pages as needed
-      };
-    },
-  };
