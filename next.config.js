c974c667f498a815b154aae03562d046cb913a71
Meghanath Shetty <meghanshetty3122@gmail.com>
2023-12-08 14:52:14 +0530

    commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Add exportPathMap configuration
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' },
+        // Add other pages as needed
+      };
+    },
+  };
