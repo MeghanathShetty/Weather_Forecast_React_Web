0d73be28141dd3f1f4aac4bf4566cf70a6d6cab7
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-04-20 13:31:25 +0530

    added icon to website

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,7 @@ function MyApp({ Component, pageProps }) {
      @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@100..900&display=swap')
           `}
         </style>
+      <link rel="icon" href="/img/web_icon.png" />
       </Helmet>
       <ToastContainer position="top-center" />
       <Component {...pageProps} />
